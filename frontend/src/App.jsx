0f8a1177862b5b51518224e223f736d715db4632
Haritha0705 <harithawikramasinha2003@gmail.com>
2025-06-05 09:26:41 +0530

    add nav bar componet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
+import NavBar from "./Components/NavBar.jsx"
 
-function App() {
-
-  return (
-      <>
-          <h1 className="text-3xl font-bold underline">
-              Hello world!
-          </h1>
-      </>
-  )
+const App = () => {
+    return (
+        <>
+            <NavBar/>
+        </>
+    )
 }
-
 export default App
